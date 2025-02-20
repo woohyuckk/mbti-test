2e812ddcc058d15780a24fbd860ecb04b11608d2
woohyuckk <woohyuckk@gmail.com>
2025-02-20 12:25:02 +0900

    feat: header 추가

--- src/shared/router.jsx
+++ src/shared/router.jsx
@@ -1,29 +1,27 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "../pages/Home"
-import Login from "../pages/Login"
-import Signup from "../pages/Signup"
-import Results from "../pages/Results"
-import Test from "../pages/Test"
-import Profile from "../pages/Profile"
-
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Results from "../pages/Results";
+import Test from "../pages/Test";
+import Profile from "../pages/Profile";
+import Layout from "../components/layout/Layout";
 
 const Router = () => {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path='/results' element={<Results/>}/>
-        <Route path='/test' element={<Test/>}/>
-        <Route path='/profile' element={<Profile/>}/>
-    </Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-)
-}
+  );
+};
 
-
-export default Router
+export default Router;

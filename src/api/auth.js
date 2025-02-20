1d48ff8f9a5a9ccc32ef3b6e3bce9c578255ffe2
woohyuckk <woohyuckk@gmail.com>
2025-02-20 19:41:58 +0900

    chore: zustand 설치

---
+++ src/api/auth.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+
+
+
+const API_URL = 'https://www.nbcamp-react-auth.link';
+
+export const register = async (userData) => { 
+  const response = await axios.post(`${API_URL}/register`, userData);
+  return response.data;
+}

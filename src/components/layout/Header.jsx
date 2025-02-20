f06e307499f9f03d438d73d2619a2166b1ffac51
woohyuckk <woohyuckk@gmail.com>
2025-02-20 15:46:34 +0900

    style: header style 수정

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
 
   return (
     <header className="bg-white border-b border-gray-200 shadow-sm">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* 좌측: Home 링크 */}
           <div className="flex-shrink-0">

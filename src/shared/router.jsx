c173bbdf8489cf7a036b2d0c10a3f274a6833207
woohyuckk <woohyuckk@gmail.com>
2025-02-25 09:06:48 +0900

    error: vercel router 설정

--- src/shared/router.jsx
+++ src/shared/router.jsx
@@ -19,7 +19,7 @@ const Router = () => {
             <Route path="/results" element={<Results />} />
             <Route path="/profile" element={<Profile />} />
           </Route>
-          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Route>
